0418c9cfcd46d5d20c7bfbb73487a4029a727ebb
Andrew Holt <drewholt12@comcast.net>
2021-06-05 18:57:42 -0500

    update logic.js for polygons

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,6 +1,3 @@
-// Add console.log ot check to see if our code is working.
-console.log("working");
-
 // We create the tile layer that will be the background of our map.
 let street = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,37 +15,42 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": street,
-  "Satelite Streets": satelliteStreets
+  "Satelite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [street]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.  baseMaps adds the key box on the web page to change between base maps
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/drewholt12/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Create a style for the lines.
-let myStyle = {
-  color: "blue",
-  weight: 1,
-  fillColor: "yellow"
-}
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-  }
-}).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
+
+
+// // Create a style for the lines.
+// let myStyle = {
+//   color: "blue",
+//   weight: 1,
+//   fillColor: "yellow"
+// }
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoHoods).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data, {
+//   style: myStyle,
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+//   }
+// }).addTo(map);
+// });
