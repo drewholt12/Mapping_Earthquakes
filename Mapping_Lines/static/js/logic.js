deca24b0f314497fa8ff47ea7ff288eb8ca1292f
Andrew Holt <drewholt12@comcast.net>
2021-06-05 14:40:33 -0500

    update lines code

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -36,15 +36,17 @@ cityData.forEach(function (city) {
 });
 // skill drill for above- changed radius to divided by 200,000... chaged color to orange, fill to orange, and lineweight to 4--  then changed to darkmap below
 
-// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the line.  FOR A SINGLE LINE SERIES.
 let line = [
   [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
 ];
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-  color: "red"
+  color: "yellow"
 }).addTo(map);
 
 //  Add a circle marker to the map for Los Angeles, California.
@@ -63,7 +65,7 @@ L.polyline(line, {
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   accessToken: API_KEY
